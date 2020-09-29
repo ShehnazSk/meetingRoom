370d584e583b9b175c2ce690f91ed110e58d134e
Shehnaz <shehnazshaik7@gmail.com>
2020-09-29 12:53:51 +0530

    fixes

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('roomApp app is running!');
+    expect(page.getTitleText()).toEqual('meetingRoom app is running!');
   });
 
   afterEach(async () => {
